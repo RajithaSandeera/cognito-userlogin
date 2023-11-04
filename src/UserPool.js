f186154ca545e6ed992fc65088fae1e36338afa3
ShanManage <lakshan2015dk@gmail.com>
2023-11-04 13:12:46 +0530

    admin create user in cognito

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js"
 
 const poolData = {
-    UserPoolId: "ap-southeast-1_h12cYNvap",
-    ClientId: "otb3tn5lcs33on43j66d1ua28"
+    UserPoolId: "ap-southeast-1_RcUYayLWn",
+    ClientId: "o8pa0qbrvg8p4jmjd1f1sp41a"
 }
 
 export default new CognitoUserPool(poolData);
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,108 @@
 import React, { useState, useContext } from 'react'
 import { AccountContext } from './Account';
 import './SignUp.css'
+import UserPool from '../UserPool';
 
-
+import { CognitoUser, CognitoUserPool } from 'amazon-cognito-identity-js';
+// import { AmazonCognitoIdentity } from 'aws-sdk'
+import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
+import { Auth } from 'aws-amplify';
 const Login = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [code, setCode] = useState('');
 
     const { authenticate } = useContext(AccountContext);
 
     const onSubmit = (event) => {
         event.preventDefault();
-        
+
         authenticate(email, password)
             .then(data => {
                 console.log("Logged in!", data);
             })
             .catch(err => {
-            console.log("Failed to login!", err);
-        })
+                console.log("Failed to login!", err);
+            })
     }
+    const onSubmitCode = (event) => {
+        event.preventDefault();
+        Auth.confirmSignUp('mas.ishara.manage@gmail.com', code)
+        .then(data => {
+            console.log('Signup confirmed successfully:', data);
+        })
+        .catch(err => {
+            console.error('Error confirming signup:', err);
+        });
+
+    }
+    const resendCode = (event) => {
+        event.preventDefault();
+        // const Username = 'mas.ishara.manage@gmail.com'
+        // const user = new CognitoUser({ Username, UserPool });
+        // user.resendConfirmationCode()
+
+        // const userPool = new CognitoUserPool(UserPool);
+
+        // const userData = {
+        // Username: 'mas.ishara.manage@gmail.com',
+        // Pool: userPool
+        // };
+
+        // const user = new CognitoUser(userData);
+        // user.resendConfirmationCode((err, result) => {
+        // if (err) {
+        //     console.error('Error resending confirmation code:', err);
+        // } else {
+        //     console.log('Confirmation code resent successfully:', result);
+        // }
+        // });
+
+
+        Auth.resendSignUp('mas.ishara.manage@gmail.com')
+        .then(data => {
+            console.log('Confirmation code resent successfully:', data);
+        })
+        .catch(err => {
+            console.error('Error resending confirmation code:', err);
+        });
+
+    }
+
+    
     return (
-        <div className="signup-container">
-          <form onSubmit={onSubmit}>
-              <label htmlFor='email'>Email</label>
-                <input
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                />
-                <label htmlFor='password'>Password</label>
-                <input
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-                <button type="submit">Login</button>
-            </form>
-    </div>
-  )
+        <>
+            <div className="signup-container">
+                <form onSubmit={onSubmit}>
+                    <label htmlFor='email'>Email</label>
+                    <input
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+            <div className="signup-container">
+                <form onSubmit={resendCode}>
+                    <button type="submit">resend Code</button>
+                </form>
+                <form onSubmit={onSubmitCode}>
+                    <label htmlFor='code'>Code</label>
+                    <input
+                        value={code}
+                        onChange={(event) => setCode(event.target.value)}
+                    />
+                    <button type="submit">Confirm</button>
+                </form>
+            </div>
+        </>
+    )
 }
 
 export default Login
