4265454aac89e28cc6860e706f092a9dc3ac8835
Rajitha Rathnayaka <rajitha.rathnayaka@acentura.com>
2023-11-05 22:51:26 +0530

    admin SignUp update

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,15 +1,28 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import UserPool from '../UserPool';
 import './SignUp.css'
+import { Auth } from 'aws-amplify';
+import AWS from 'aws-sdk'
 
 const SignUp = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    
+    
+    // const AWS = require('aws-sdk');
 
-    const onSubmit = (event) => {
+    AWS.config.update({
+    region: 'ap-southeast-1', // Replace with your AWS region
+    accessKeyId: 'AKIAX6P5VLDC4Q2PVMJA', // Replace with your access key ID
+    secretAccessKey: '3qlGUYbgRNTEjYWmmQkaaAHQW4c3k/vMLfuSZlK2' // Replace with your secret access key
+    });
+
+    const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+
+    const onSubmit = async (event) => {
         event.preventDefault();
-        
+
         UserPool.signUp(email, password, [], null, (err, data) => {
             if (err) {
                 console.log(err);
@@ -17,11 +30,61 @@ const SignUp = () => {
                 console.log("successfully login:", data);
             }
         }); 
+
+
+
+        // ADMIN CREATE COGNITO 
+
+        const params = {
+            UserPoolId: 'ap-southeast-1_RcUYayLWn', 
+            // UserPoolId: "ap-southeast-1_h12cYNvap",
+            Username: email, 
+            TemporaryPassword: password,
+            UserAttributes: [
+                {
+                Name: 'email',
+                Value: email 
+                },
+                {
+                Name: 'custom:mustChangePassword',
+                Value: 'true'
+                }
+            ],
+            MessageAction: 'SUPPRESS' 
+        };
+
+        cognitoidentityserviceprovider.adminCreateUser(params, function(createUserErr, createUserData) {
+            if (createUserErr) {
+              console.error('Error creating user:', createUserErr);
+            } else {
+              console.log('User created successfully:', createUserData);
+              
+              // After successful creation, send the invitation email
+              const resendParams = {
+                UserPoolId: 'ap-southeast-1_RcUYayLWn',
+                // UserPoolId: "ap-southeast-1_h12cYNvap",
+                Username: email,
+                MessageAction: 'RESEND'
+              };
+          
+              cognitoidentityserviceprovider.adminCreateUser(resendParams, function(resendErr, resendData) {
+                if (resendErr) {
+                  console.error('Error resending invitation:', resendErr);
+                } else {
+                  console.log('Invitation email resent successfully:', resendData);
+                }
+              });
+            }
+          });
     }
+
+
+
+
     return (
         <div className="signup-container">
-          <form onSubmit={onSubmit}>
-              <label htmlFor='email'>Email</label>
+            <form onSubmit={onSubmit}>
+                <label htmlFor='email'>Email</label>
                 <input
                     value={email}
                     onChange={(event) => setEmail(event.target.value)}
@@ -35,8 +98,8 @@ const SignUp = () => {
                 </input>
                 <button type="submit">SignUp</button>
             </form>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default SignUp
